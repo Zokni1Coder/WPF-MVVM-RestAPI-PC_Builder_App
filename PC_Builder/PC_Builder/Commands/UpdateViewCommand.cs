462587f5d1fda90529347d0e96bd4b00bf9e573a
erikk <erikk@ERIKK>
2025-03-03 16:03:34 +0100

    Add project files.

---
+++ PC_Builder/PC_Builder/Commands/UpdateViewCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PC_Builder.ViewModels;
+
+namespace PC_Builder.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainWindowViewModel viewModel;
+        public UpdateViewCommand(MainWindowViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+
+            switch (parameter.ToString())
+            {
+                case "Home":
+                    viewModel.SelectedViewModel = new HomeViewModel();
+                    break;
+                case "Motherboard":
+                    viewModel.SelectedViewModel = new MotherboardViewModel();
+                    break;
+                case "CPU":
+                    viewModel.SelectedViewModel = new CPUViewModel();
+                    break;
+                case "GPU":
+                    viewModel.SelectedViewModel = new GPUViewModel();
+                    break;
+                case "CPU Cooler":
+                    viewModel.SelectedViewModel = new CPUCoolerViewModel();
+                    break;
+                case "RAM":
+                    viewModel.SelectedViewModel = new RAMViewModel();
+                    break;
+                case "ROM":
+                    viewModel.SelectedViewModel = new ROMViewModel();
+                    break;
+                case "PS":
+                    viewModel.SelectedViewModel = new PowerSupplyViewModel();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ PC_Builder/PC_Builder/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using PC_Builder.Commands;
+
+namespace PC_Builder.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+		private BaseViewModel selectedViewModel = new HomeViewModel();
+
+		public BaseViewModel SelectedViewModel
+		{
+			get { return selectedViewModel; }
+			set { 
+                selectedViewModel = value; 
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+		}
+
+
+		public ICommand UpdateViewCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
