a5d6eb003e0540c1ada8c8847789e83882be1354
ErikKovács <148643578+Zokni1Coder@users.noreply.github.com>
2025-03-04 11:23:03 +0100

    motherboard APIs

--- node-rest-pc_parts/api/routes/motherboards.js
+++ node-rest-pc_parts/api/routes/motherboards.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 
 router.get('/', async (req, res) => {
     try {
-        const [rows] = await db.query('SELECT * FROM motherboard');
+        const [rows] = await db.query('SELECT motherboard.id, manufacturer.name AS manufacturer, motherboard.info, cpu_socket.name AS socket, form_factor_motherboard.form AS form_factor, motherboard_chipset.name AS chipset, motherboard.memory_max, ram_types.type AS ram_type, motherboard.memory_slots_no, motherboard.sata_60gbs_no, motherboard.onboard_ethernet, motherboard.wifi, motherboard.raid_supp, motherboard.price FROM motherboard JOIN manufacturer ON motherboard.manufacturer_id = manufacturer.id JOIN form_factor_motherboard ON motherboard.form_factor_id = form_factor_motherboard.id JOIN cpu_socket ON motherboard.socket_id = cpu_socket.id JOIN motherboard_chipset ON motherboard.chipset_id = motherboard_chipset.id JOIN ram_types ON motherboard.memory_type_id = ram_types.id');
         res.status(200).json(rows);
     } catch (error) {
         console.error("SQL Error:", error); 
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const [rows] = await db.query('SELECT * FROM motherboard WHERE id = ?', [id]);
+        const [rows] = await db.query('SELECT motherboard.id, manufacturer.name AS manufacturer, motherboard.info, cpu_socket.name AS socket, form_factor_motherboard.form AS form_factor, motherboard_chipset.name AS chipset, motherboard.memory_max, ram_types.type AS ram_type, motherboard.memory_slots_no, motherboard.sata_60gbs_no, motherboard.onboard_ethernet, motherboard.wifi, motherboard.raid_supp, motherboard.price FROM motherboard JOIN manufacturer ON motherboard.manufacturer_id = manufacturer.id JOIN form_factor_motherboard ON motherboard.form_factor_id = form_factor_motherboard.id JOIN cpu_socket ON motherboard.socket_id = cpu_socket.id JOIN motherboard_chipset ON motherboard.chipset_id = motherboard_chipset.id JOIN ram_types ON motherboard.memory_type_id = ram_types.id WHERE motherboard.id = ?', [id]);
 
         if (rows.length === 0) {
             return res.status(404).json({ error: "Not found anything" });
