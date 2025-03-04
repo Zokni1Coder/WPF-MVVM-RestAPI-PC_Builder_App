a96d096d0316d7bd9df1dba1facc3c6d622f7ee5
ErikKovács <148643578+Zokni1Coder@users.noreply.github.com>
2025-03-04 11:47:33 +0100

    cpu_cooler-APIs

--- node-rest-pc_parts/app.js
+++ node-rest-pc_parts/app.js
@@ -1,11 +1,14 @@
 const express = require('express');  //ez egy csomag. installaltuk a terminalban
 const app = express(); //executolja az express csomagot
 const motherboardRoutes = require('./api/routes/motherboards'); //mivel itt megadtuk, ezert nem kell a motherboard.js-ben megadni, csak a "\" jelet.
+const cpu_coolerRoutes = require('./api/routes/cpu_coolers');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/motherboards', motherboardRoutes); // ennek koszonhetoen minden ami ezzel kezdodik url tovabbitva lesz a motherboard.js-be
+app.use('/cpu_coolers', cpu_coolerRoutes);
+
 
 module.exports = app;
