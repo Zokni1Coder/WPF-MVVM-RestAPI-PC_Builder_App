73995ae33cf56f0fe9dc981db2ba1f734f8a72df
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-05 16:59:30 +0100

    get APIs - ram

--- node-rest-pc_parts/app.js
+++ node-rest-pc_parts/app.js
@@ -5,6 +5,7 @@ const cpu_coolerRoutes = require('./api/routes/cpu_coolers');
 const cpuRoutes = require('./api/routes/cpus');
 const gpuRoutes = require('./api/routes/gpus');
 const power_suppliesRoutes = require('./api/routes/power_supplies');
+const ramRoutes = require('./api/routes/rams');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -14,6 +15,7 @@ app.use('/motherboards', motherboardRoutes); // ennek koszonhetoen minden ami ez
 app.use('/cpu_coolers', cpu_coolerRoutes);
 app.use('/cpus', cpuRoutes);
 app.use('/gpus', gpuRoutes);
+app.use('/rams', ramRoutes);
 app.use('/power_supplies', power_suppliesRoutes);
 
 
