987235dd9660bd3733665038cd38dbea463e1ccd
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-05 16:29:44 +0100

    get APIs - gpu

--- node-rest-pc_parts/app.js
+++ node-rest-pc_parts/app.js
@@ -2,7 +2,8 @@ const express = require('express');  //ez egy csomag. installaltuk a terminalban
 const app = express(); //executolja az express csomagot
 const motherboardRoutes = require('./api/routes/motherboards'); //mivel itt megadtuk, ezert nem kell a motherboard.js-ben megadni, csak a "\" jelet.
 const cpu_coolerRoutes = require('./api/routes/cpu_coolers');
-const cpuRoutes = require('./api/routes/cpu');
+const cpuRoutes = require('./api/routes/cpus');
+const gpuRoutes = require('./api/routes/gpus');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use('/motherboards', motherboardRoutes); // ennek koszonhetoen minden ami ezzel kezdodik url tovabbitva lesz a motherboard.js-be
 app.use('/cpu_coolers', cpu_coolerRoutes);
 app.use('/cpus', cpuRoutes);
+app.use('/gpus', gpuRoutes);
 
 
 module.exports = app;
