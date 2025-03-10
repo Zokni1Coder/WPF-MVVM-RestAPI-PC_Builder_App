423c48a8cbb6331db453d592700d75e20f0d7611
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-11 00:09:26 +0100

    cpu select correction

--- node-rest-pc_parts/api/routes/cpus.js
+++ node-rest-pc_parts/api/routes/cpus.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 
 router.get('/', async (req, res) => {
     try {
-        const [cpus] = await db.query('SELECT cpu.id, manufacturer.name, cpu_series.name AS series, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id');
+        const [cpus] = await db.query('SELECT cpu.id, manufacturer.name AS manufacturer, cpu_series.name AS series, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id');
         res.status(200).json(cpus);
     } catch (error) {
         console.error("SQL Error:", error); 
