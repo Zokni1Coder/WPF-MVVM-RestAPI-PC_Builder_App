5832621b1cb7fe26b6d567a14c35c7131841c647
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-11 17:57:26 +0100

    cpu - select correction

--- node-rest-pc_parts/api/routes/cpus.js
+++ node-rest-pc_parts/api/routes/cpus.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 
 router.get('/', async (req, res) => {
     try {
-        const [cpus] = await db.query('SELECT cpu.id, manufacturer.name AS manufacturer, cpu_series.name AS series, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id');
+        const [cpus] = await db.query('SELECT cpu.id, manufacturer.name AS manufacturer, cpu_series.name AS series, cpu.model, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id');
         res.status(200).json(cpus);
     } catch (error) {
         console.error("SQL Error:", error); 
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const [cpu] = await db.query('SELECT cpu.id, manufacturer.name, cpu_series.name AS series, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id WHERE cpu.id = ?', [id]);
+        const [cpu] = await db.query('SELECT cpu.id, manufacturer.name AS manufacturer, cpu_series.name AS series, cpu.model, cpu_microarchitecture.name AS microarchitecture, cpu_socket.name AS socket, cpu.core_count, cpu.thread_count, cpu.core_clock, cpu.boost_core_clock, cpu.L2_cache, cpu.L3_cache, cpu.tdp, integrated_graphics.model AS integrated_graphics, cpu.price from cpu JOIN cpu_series ON cpu_series.id = cpu.series_id JOIN cpu_microarchitecture ON cpu_microarchitecture.id = cpu.microarchitecture_id JOIN cpu_socket ON cpu_socket.id = cpu.socket_id JOIN manufacturer ON cpu_series.manufacturer_id = manufacturer.id LEFT JOIN integrated_graphics ON integrated_graphics.id = cpu.integrated_gpu_id WHERE cpu.id = ?', [id]);
         res.status(200).json(cpu);
     } catch (error) {
         console.error("SQL Hiba:", error);
