50ab42d75d7559ffe76607ede3b80848c15bb20b
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-11 18:47:32 +0100

    GPU - model

---
+++ PC_Builder/PC_Builder/Models/GPU.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PC_Builder.Models
+{
+    public class GPU
+    {
+        private int id;
+        [JsonPropertyName("id")]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string info;
+        [JsonPropertyName("info")]               
+        public string Info
+        {
+            get { return info; }
+            set { info = value; }
+        }
+
+        private string manufacturer;
+        [JsonPropertyName("manufacturer")]
+        public string Manufacturer
+        {
+            get { return manufacturer; }
+            set { manufacturer = value; }
+        }
+
+        private string brand;
+        [JsonPropertyName("brand")]
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+        }
+
+        private string technology;
+        [JsonPropertyName("technology")]
+        public string Technology
+        {
+            get { return technology; }
+            set { technology = value; }
+        }
+
+        private int model;
+        [JsonPropertyName("model")]
+        public int Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        private int vram;
+        [JsonPropertyName("vram")]
+        public int Vram
+        {
+            get { return vram; }
+            set { vram = value; }
+        }
+
+        private string ram_type;
+        [JsonPropertyName("ram_type")]
+        public string Ram_type
+        {
+            get { return ram_type; }
+            set { ram_type = value; }
+        }
+
+        private int core_clock;
+        [JsonPropertyName("core_clock")]
+        public int core_clock
+        {
+            get { return core_clock; }
+            set { core_clock = value; }
+        }
+
+        private int boost_clock;
+        [JsonPropertyName("boost_clock")]
+        public int boost_clock
+        {
+            get { return boost_clock; }
+            set { boost_clock = value; }
+        }
+
+        private string Interface;
+        [JsonPropertyName("interface")]
+        public string INterface
+        {
+            get { return Interface; }
+            set { Interface = value; }
+        }
+
+        private string frame_sync;
+        [JsonPropertyName("frame_sync")]
+        public string Frame_sync
+        {
+            get { return frame_sync; }
+            set { frame_sync = value; }
+        }
+
+        private int tdp;
+        [JsonPropertyName("tdp")]
+        public int Tdp
+        {
+            get { return tdp; }
+            set { tdp = value; }
+        }
+
+        private int hdmi_ouput;
+        [JsonPropertyName("hdmi_ouput")]
+        public int hdmi_ouput
+        {
+            get { return hdmi_ouput; }
+            set { hdmi_ouput = value; }
+        }
+
+        private int dp_port_output;
+        [JsonPropertyName("dp_port_output")]
+        public int Dp_port_output
+        {
+            get { return dp_port_output; }
+            set { dp_port_output = value; }
+        }
+
+        private int price;
+        [JsonPropertyName("price")]
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+    }
+}
