29fbe0ce44b610e86e12b901ed7434c116e5cf01
Erik Kovacs <erikkovacs0000@gmail.com>
2025-03-12 00:52:06 +0100

    power_supply - get API correction

--- node-rest-pc_parts/api/routes/power_supplies.js
+++ node-rest-pc_parts/api/routes/power_supplies.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 
 router.get('/', async (req, res) => {
     try {
-        const [pss] = await db.query('SELECT ps.id, ps.model, manufacturer.name AS manufacturer, power_supply_types.type, power_supply_ratings.rating, ps.wattage, power_supply_modularity.modularity, ps.wattage FROM power_supply AS ps JOIN manufacturer ON manufacturer.id = ps.manufacturer JOIN power_supply_types ON power_supply_types.id = ps.type JOIN power_supply_ratings ON power_supply_ratings.id = ps.efficiency_rating JOIN power_supply_modularity ON power_supply_modularity.id = ps.modularity');
+        const [pss] = await db.query('SELECT ps.id, ps.model, manufacturer.name AS manufacturer, power_supply_types.type, power_supply_ratings.rating, ps.wattage, power_supply_modularity.modularity, ps.wattage, ps.price FROM power_supply AS ps JOIN manufacturer ON manufacturer.id = ps.manufacturer JOIN power_supply_types ON power_supply_types.id = ps.type JOIN power_supply_ratings ON power_supply_ratings.id = ps.efficiency_rating JOIN power_supply_modularity ON power_supply_modularity.id = ps.modularity');
         res.status(200).json(pss);
     } catch (error) {
         console.error("SQL Error:", error); 
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const [ps] = await db.query('SELECT ps.id, ps.model, manufacturer.name AS manufacturer, power_supply_types.type, power_supply_ratings.rating, ps.wattage, power_supply_modularity.modularity, ps.wattage FROM power_supply AS ps JOIN manufacturer ON manufacturer.id = ps.manufacturer JOIN power_supply_types ON power_supply_types.id = ps.type JOIN power_supply_ratings ON power_supply_ratings.id = ps.efficiency_rating JOIN power_supply_modularity ON power_supply_modularity.id = ps.modularity WHERE ps.id = ?', [id]);
+        const [ps] = await db.query('SELECT ps.id, ps.model, manufacturer.name AS manufacturer, power_supply_types.type, power_supply_ratings.rating, ps.wattage, power_supply_modularity.modularity, ps.wattage, ps.price FROM power_supply AS ps JOIN manufacturer ON manufacturer.id = ps.manufacturer JOIN power_supply_types ON power_supply_types.id = ps.type JOIN power_supply_ratings ON power_supply_ratings.id = ps.efficiency_rating JOIN power_supply_modularity ON power_supply_modularity.id = ps.modularity WHERE ps.id = ?', [id]);
         res.status(200).json(ps);
     } catch (error) {
         console.error("SQL Hiba:", error);
